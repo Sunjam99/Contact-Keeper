58691d4414cd0b410aaf9a29cbf4ed0c64791280
SimbaMusaz99 <simzmusaz@gmail.com>
2020-06-23 02:58:49 +0200

    'Enter'

--- routes/auth.js
+++ routes/auth.js
@@ -6,7 +6,7 @@ const config = require('config');
 const auth = require('../middleware/auth');
 const {check, validationResult} = require('express-validator');
 
-const User = require('../models/User');
+const User = require('./modals/user');
 
 
 router.get('/', auth, async (req, res) => {
--- routes/config/default.js
+++
@@ -1,4 +0,0 @@
-{
-    "mongoURI":"mongodb+srv://sunjam99:simbamusanjeya@contactskepper-owens.mongodb.net/ContactsKeeper?retryWrites=true&w=majority",
-    "jwtSecret":"secret"
-}
